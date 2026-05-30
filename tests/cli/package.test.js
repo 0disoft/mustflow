fb1664d1ba765d225612a762c436478ce85a734e
0disoft <rodisoft1@gmail.com>
2026-05-30 20:46:03 +0900

    Bump mustflow package version to 2.22.47

--- tests/cli/package.test.js
+++ tests/cli/package.test.js
@@ -77,7 +77,7 @@ function readProjectText(relativePath) {
 }
 
 test('package metadata is ready for public npm publishing', () => {
-	assert.equal(packageJson.version, '2.22.46');
+	assert.equal(packageJson.version, '2.22.47');
 	assert.equal(packageJson.license, 'MIT-0');
 	assert.equal(packageJson.homepage, 'https://0disoft.github.io/mustflow/');
 	assert.deepEqual(packageJson.repository, {
