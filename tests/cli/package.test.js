86f2b6b87bd61b74ada503f79366a1df2b7ae408
0disoft <zerodi0000@gmail.com>
2026-07-05 17:53:24 +0900

    feat(skills): expand writing elegance phrase bank

--- tests/cli/package.test.js
+++ tests/cli/package.test.js
@@ -48,7 +48,7 @@ function readProjectText(relativePath) {
 }
 
 test('package metadata is ready for public npm publishing', () => {
-	assert.equal(packageJson.version, '2.112.0');
+	assert.equal(packageJson.version, '2.112.1');
 	assert.equal(packageJson.license, 'MIT-0');
 	assert.equal(packageJson.homepage, 'https://0disoft.github.io/mustflow/');
 	assert.deepEqual(packageJson.repository, {
